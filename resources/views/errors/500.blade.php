88d25f85fde73db1bc90c00eaa18b0cd25400dca
Ronald <sikk0003@hz.nl>
2022-06-26 20:08:36 +0200

    404 edit

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -13,10 +13,10 @@
     <img style="width: 150px; height: 60px;" src="/img/logo.png">
 </a>
 <h1 style="text-align: center; font-weight: bold; font-size: 100px;">500</h1>
-<h2 style="text-align: center; font-size: 30px;">Oeps, het lijkt erop dat deze pagina momenteel niet werkt.<br>Klik hieronder om terug te gaan naar de hoofdpagina.</h2>
+<h2 style="text-align: center; font-size: 30px;">Sorry, het ligt niet aan jou, maar aan ons...<br></h2>
 <a href="/">
-    <h1 style="text-align: center; font-weight: bold; font-size: 60px; color: black;">Hoofdpagina</h1>
+    <h1 style="text-align: center; font-weight: bold; font-size: 60px; color: greenyellow;">>Snel, terug naar de bewoonde wereld!<</h1>
 </a>
-<img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="/img/brokenhanger2.jpg" alt="Broken Hanger">
+<img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="/img/broken.jpg" alt="Broken">
 </body>
 </html>
