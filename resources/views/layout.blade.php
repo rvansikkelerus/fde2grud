7f0d95eb254058d9e68be3ff5d1800c98ee18ea0
Ronald <sikk0003@hz.nl>
2022-06-26 13:37:45 +0200

    edit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <title>Kledingbank Project</title>
+    <title> GGD Zeeland registratie</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
     </head>
     <body>
